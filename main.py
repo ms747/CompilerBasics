b4a4bf5e16b47e7c1fa65138d0098414cc08b011
ms747 <mayur.shah.ha@gmail.com>
2021-06-22 10:33:28 +0530

    Add: Tokenizer

--- main.py
+++ main.py
@@ -1,57 +1,6 @@
 from types import *
 from parsing import *
-
-class Tokenizer:
-    def __init__(self, buf):
-        self.buf   = buf
-        self.index = 0
-
-    def token(self):
-        return self.buf[self.index]
-
-    def move(self, value):
-        self.index += value
-
-    def skip_whitespaces(self):
-        while self.index < len(self.buf) and self.token().isspace():
-            self.move(1)
-
-    def next(self):
-        self.skip_whitespaces()
-
-        if self.index < len(self.buf):
-            if self.token() == '+':
-                self.move(1)
-                return Symbol('+')
-            elif self.token() == '-':
-                self.move(1)
-                return Symbol('-')
-            elif self.token() == '*':
-                self.move(1)
-                return Symbol('*')
-            elif self.token() == '/':
-                self.move(1)
-                return Symbol('/')
-            elif self.token() == '(':
-                self.move(1)
-                return OpenParen()
-            elif self.token() == ')':
-                self.move(1)
-                return CloseParen()
-            else:
-                if self.token().isnumeric():
-                    number = int(self.token())
-                    self.move(1)
-                    while self.index < len(self.buf) and self.token().isnumeric():
-                        number = number * 10 + int(self.token())
-                        self.move(1)
-                    return Number(number)
-                else:
-                    char = self.token()
-                    self.move(1)
-                    return Undefined(char)
-        else:
-            return Eof()
+from tokenizer import *
 
 tokenizer = Tokenizer("9 * 2 / 2")
 tokens = []
---
+++ tokenizer.py
@@ -0,0 +1,53 @@
+from types import *
+
+class Tokenizer:
+    def __init__(self, buf):
+        self.buf   = buf
+        self.index = 0
+
+    def token(self):
+        return self.buf[self.index]
+
+    def move(self, value):
+        self.index += value
+
+    def skip_whitespaces(self):
+        while self.index < len(self.buf) and self.token().isspace():
+            self.move(1)
+
+    def next(self):
+        self.skip_whitespaces()
+
+        if self.index < len(self.buf):
+            if self.token() == '+':
+                self.move(1)
+                return Symbol('+')
+            elif self.token() == '-':
+                self.move(1)
+                return Symbol('-')
+            elif self.token() == '*':
+                self.move(1)
+                return Symbol('*')
+            elif self.token() == '/':
+                self.move(1)
+                return Symbol('/')
+            elif self.token() == '(':
+                self.move(1)
+                return OpenParen()
+            elif self.token() == ')':
+                self.move(1)
+                return CloseParen()
+            else:
+                if self.token().isnumeric():
+                    number = int(self.token())
+                    self.move(1)
+                    while self.index < len(self.buf) and self.token().isnumeric():
+                        number = number * 10 + int(self.token())
+                        self.move(1)
+                    return Number(number)
+                else:
+                    char = self.token()
+                    self.move(1)
+                    return Undefined(char)
+        else:
+            return Eof()
